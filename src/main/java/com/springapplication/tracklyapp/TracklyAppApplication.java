09eada0de69fe386c252847ea4ad0441939eb3dd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-04 16:44:37 +0100

    Create a skeleton Trackly App project

---
+++ src/main/java/com/springapplication/tracklyapp/TracklyAppApplication.java
@@ -0,0 +1,13 @@
+package com.springapplication.tracklyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracklyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracklyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springapplication/tracklyapp/TracklyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapplication.tracklyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracklyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
