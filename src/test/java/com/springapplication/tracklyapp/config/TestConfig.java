d2bcaa27d85307cc1977cd80886ff198a59e1830
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-26 20:01:01 +0100

    Refactor TestConfig class

---
+++ src/test/java/com/springapplication/tracklyapp/config/TestConfig.java
@@ -0,0 +1,17 @@
+package com.springapplication.tracklyapp.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@TestConfiguration
+@Profile({"test", "ci", "test-seed"})
+public class TestConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
