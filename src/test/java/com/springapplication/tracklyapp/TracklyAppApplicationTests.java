6d96924b22eef5b036a1d0dde7e1dfc36f180a9f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-26 20:02:09 +0100

    Refactor TracklyAppApplicationTests class

--- src/test/java/com/springapplication/tracklyapp/TracklyAppApplicationTests.java
+++ src/test/java/com/springapplication/tracklyapp/TracklyAppApplicationTests.java
@@ -3,6 +3,7 @@ package com.springapplication.tracklyapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.sql.DataSource;
@@ -11,6 +12,7 @@ import java.sql.Connection;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 @TestPropertySource("classpath:application-ci.properties")
 class TracklyAppApplicationTests {
 
@@ -20,6 +22,7 @@ class TracklyAppApplicationTests {
     @Test
     void contextLoads() {
         // Ensures Spring Boot context starts
+        assertThat(dataSource).isNotNull();
     }
 
     @Test
@@ -27,8 +30,7 @@ class TracklyAppApplicationTests {
         try (Connection conn = dataSource.getConnection()) {
             assertThat(conn).isNotNull();
             assertThat(conn.isValid(2)).isTrue();
-            System.out.println("✅ Successfully connected to PostgreSQL in CI workflow.");
+            System.out.println("Successfully connected to PostgreSQL in CI workflow.");
         }
     }
-
 }
