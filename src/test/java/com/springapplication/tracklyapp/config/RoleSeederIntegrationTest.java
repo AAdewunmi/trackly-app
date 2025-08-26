cdada1f180a443e2c0facbc5b90c03f9dec9ad44
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-26 20:01:39 +0100

    Refactor RoleSeederIntegrationTest class

--- src/test/java/com/springapplication/tracklyapp/config/RoleSeederIntegrationTest.java
+++ src/test/java/com/springapplication/tracklyapp/config/RoleSeederIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Works whether roles are provided by the Java seeder or by Flyway SQL.
  */
 @SpringBootTest
+@ActiveProfiles("ci")
 class RoleSeederIntegrationTest {
 
     @Autowired
