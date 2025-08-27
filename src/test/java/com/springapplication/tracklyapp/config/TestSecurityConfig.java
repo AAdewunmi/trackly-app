a9e498f43a36b8248449a42ddc0018a81c38a26a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-08-27 10:59:56 +0100

    Implement TestSecuirtyConfig class

---
+++ src/test/java/com/springapplication/tracklyapp/config/TestSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.springapplication.tracklyapp.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Disables security for MVC controller tests to allow public access.
+ */
+@TestConfiguration
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
+
